63db70d1c282fe73185d222b81f27826b095b3cd
Daniel Sowers <daniel@m2mbp.cinci.rr.com>
2024-03-15 02:56:25 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import subprocess
+import speedtest
+import platform
+
+os = platform.system()
+
+#This function will give the user the name of the network
+def network_name():
+    name = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport", "-I"]).decode('utf-8').split("\n")[12]
+    print("SSID: " + name.split(": ")[1])
+
+
+def get_speed():
+    st = speedtest.Speedtest()
+    dow = st.download()
+    print("Download speed: " + str(int(dow) / 1000000) + " bytes\n")
+
+
+
+
+while True:
+    print(os)
+    inp = input("1 - View name of the current network\n"
+                "2 - View the speed of the current network\n")
+    if inp == "1":
+        network_name()
+    if inp == "2":
+        get_speed()
+
