07fd55c8c4041cb28fe1f1e152416cb93202b0e3
Daniel Sowers <daniel@m2mbp.local>
2024-03-19 16:35:14 -0400

    Implementing OS specific design

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ os = platform.system()
 
 #This function will give the user the name of the network
 def network_name():
-    name = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport", "-I"]).decode('utf-8').split("\n")[12]
+    if (os == "Darwin"):
+        name = subprocess.check_output(["/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport", "-I"]).decode('utf-8').split("\n")[12]
     print("SSID: " + name.split(": ")[1])
 
 
 def get_speed():
     st = speedtest.Speedtest()
     dow = st.download()
-    print("Download speed: " + str(int(dow) / 1000000) + " bytes\n")
+    print("Download speed: " + str(int(dow)))
 
 
 
 
 while True:
-    print(os)
     inp = input("1 - View name of the current network\n"
                 "2 - View the speed of the current network\n")
     if inp == "1":
